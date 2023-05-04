bba5c2e9b02ffe4fb37489c85d70e655c3bdab6a
Van Lam <lamvanr@gmail.com>
2023-05-04 15:32:48 +0700

    Add project files.

---
+++ Animal/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("20 kg", "1.5", "Kitty");
+            cat.PrintInfo();
+        }
+    }
+}
